69fd756aa0897193e16c292d370b3a8672519a55
jihye <jiji.pak41@gmail.com>
2021-09-01 00:24:28 +0100

    complete add list function

--- script.js
+++ script.js
@@ -4,12 +4,24 @@
 
 const add_btn = document.querySelector('.button__add');
 const task = document.querySelector('#task__input');
+const ul = document.querySelector('.main__body > ul');
 
 let new_task = '';
 
 function addList(e) {
   e.preventDefault();
   console.log('clicked');
+
+  const template = document.querySelector('#list__template');
+  const dom_frag = template.content.cloneNode(true);
+
+  dom_frag.querySelector('.item__checklist').innerHTML =
+    '<input type="checkbox">';
+  dom_frag.querySelector('.item__name').textContent = new_task;
+  dom_frag.querySelector('.item__bin').innerHTML =
+    '<i class="far fa-trash-alt"></i>';
+  ul.append(dom_frag);
+  task.value = '';
 }
 
 function updateText() {
