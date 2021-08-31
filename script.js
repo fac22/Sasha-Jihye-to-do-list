e8aab9fb8dfb8a76984465231e45f3d5ea35b76a
jihye <jiji.pak41@gmail.com>
2021-09-01 00:13:54 +0100

    create function to get input value

--- script.js
+++ script.js
@@ -3,10 +3,20 @@
 // button click event
 
 const add_btn = document.querySelector('.button__add');
+const task = document.querySelector('#task__input');
+
+let new_task = '';
 
 function addList(e) {
   e.preventDefault();
   console.log('clicked');
 }
 
+function updateText() {
+  new_task = task.value;
+  console.log(new_task);
+}
+
 add_btn.addEventListener('click', addList);
+
+task.addEventListener('change', updateText);
