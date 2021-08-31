9d38921fac0b089923d8206c8417ec42c0452b1a
jihye <jiji.pak41@gmail.com>
2021-09-01 00:46:40 +0100

    add comments

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 'use strict';
 
-// button click event
-
 const add_btn = document.querySelector('.button__add');
 const task = document.querySelector('#task__input');
 const ul = document.querySelector('.main__body > ul');
 
 let new_task = '';
 
+// function - add each task when add button is clicked
+
 function addList(e) {
   e.preventDefault();
   console.log('clicked');
 
+  // clone template's elements
   const template = document.querySelector('#list__template');
   const dom_frag = template.content.cloneNode(true);
 
+  // only update input value when the length is greater than 0
   if (new_task.length > 0) {
+    // update each template element 1) checkbox, 2) task, 3) bin
     dom_frag.querySelector('.item__checklist').innerHTML =
       '<input type="checkbox">';
     dom_frag.querySelector('.item__name').textContent = new_task;
     dom_frag.querySelector('.item__bin').innerHTML =
       '<i class="far fa-trash-alt"></i>';
+
+    // append dom frag to ul
     ul.append(dom_frag);
+
+    // reset task value
     task.value = '';
 
+    // grab all bin icons
     const bin_btn = Array.from(document.querySelectorAll('.item__bin'));
 
+    // add eventlistener to remove the task when the bin is clicked
     bin_btn.forEach((e) =>
       e.addEventListener('click', () => {
         e.parentElement.remove();
@@ -34,9 +43,11 @@ function addList(e) {
     );
   }
 
+  // reset new_task value
   new_task = '';
 }
 
+// create function to get task value
 function updateText() {
   new_task = task.value;
   console.log(new_task);
